77926b2f0b8037b2b63dbe42e945b44de83d5950
suhib-kharoush <suhaib_kharwash@outlook.com>
2021-01-28 14:44:42 +0200

    add loops

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ alert('welcome ' + userName);
 var age = prompt('how older you?');
 
 
+
+
 var seasons = prompt('what is the season you prefer?');
 console.log(seasons);
 if (seasons == 'summer') {
@@ -19,5 +21,23 @@ if (seasons == 'summer') {
 alert(message + userName)
 
 
-document.write('<h1>' + message + userName +'<h1>');
+document.write('<h1>' + message + userName + '<h1>');
+
+var order = prompt('what is the contry you planing to visit?');
+while(order !== 'turkey' && order !== 'greece' && order !== 'croatia'){
+    order = prompt('we are sorry our servises just in greece, turkey and croatia, if you interested to any one of them please write it here ')
+}
+
+var country = ''
+if (order === 'greece') {country ='<img src="images/greece.jpg" />';
+    
+}else if (order === 'turkey') {country ='<img src="images/turkey.jpg" />"';
+    
+}else if (order === 'croatia') {country ='<img src="images/croatia.jpg" />';
+    
+}
+document.write(country);
+for (var i = 0 ; i<7 ; i++){
+    document.write('welcome to ' + country);
+}
 
